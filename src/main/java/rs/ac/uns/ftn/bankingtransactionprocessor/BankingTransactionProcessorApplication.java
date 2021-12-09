e63c737f0ce251bcc4d360cb219ec831f49ccb08
aleksandarhavran <salehavran516@gmail.com>
2021-12-09 19:08:57 +0100

    Initial commit

---
+++ src/main/java/rs/ac/uns/ftn/bankingtransactionprocessor/BankingTransactionProcessorApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.bankingtransactionprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransactionProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingTransactionProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/uns/ftn/bankingtransactionprocessor/BankingTransactionProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.bankingtransactionprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTransactionProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
