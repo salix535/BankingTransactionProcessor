1a8811dc6d1b622769b3e58835d38c0918320aa4
aleksandarhavran <salehavran516@gmail.com>
2022-01-02 19:30:50 +0100

    Date formatting

--- src/main/java/rs/ac/uns/ftn/bankingtransactionprocessor/transaction/TransactionService.java
+++ src/main/java/rs/ac/uns/ftn/bankingtransactionprocessor/transaction/TransactionService.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.time.ZonedDateTime;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -30,7 +30,7 @@ public class TransactionService {
     private String queueUrl;
 
     public void processTransactionAndSendToSqs(final TransactionRequest transactionRequest, final String userName) {
-        transactionRequest.setCreationTime(ZonedDateTime.now());
+        transactionRequest.setCreationTime(LocalDateTime.now());
         transactionRequest.setCreatedBy(userName);
         try {
             sqs.sendMessage(queueUrl, objectMapper.writeValueAsString(transactionRequest));
